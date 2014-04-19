cc0112dff6ab4cc5223a86cbab2870da038eee51
Gerit Wagner <gerit1989@googlemail.com>
2014-04-19 18:09:22 +0200

    Ausschreibung aktualisiert

--- ausschreibung.php
+++ ausschreibung.php
@@ -2,7 +2,7 @@
  $menu['current'] = "ausschreibung"; ?>
 <?php require("_header.inc.php"); ?>
 
-<h2>Sparkassen Gala 2013</h2>
+<h2>Sparkassen Gala 2014</h2>
 <br />
 
 <strong>mit LG Telis Finanz Laufnacht</strong>
@@ -25,10 +25,11 @@
 </tr>
 <tr>
   <th>Termin:</th>
-  <td>Samstag, 08. Juni 2013<br />
-    Beginn Vorprogramm 10.00 Uhr<br />
-    Beginn Hauptprogramm 16.00 Uhr <br />
-    Laufnacht 19.30 Uhr </td>
+  <td>Samstag, 07. Juni 2014: Sparkassen-Gala<br />
+    Beginn Vorprogramm 13.00 Uhr<br />
+    Beginn Hauptprogramm 16.00 Uhr <br /></td>
+  <td>Samstag, 06. Juni 2014: Laufnacht<br />
+    Beginn Laufnacht 18.00 Uhr </td>
 </tr>
 <tr>
   <th>Meldungen:</th>
@@ -39,77 +40,65 @@
   E-Mail <a href="mailto:meldung@lg-regensburg.de">meldung@lg-regensburg.de</a><br />
   <br />
   Die Meldung ist auch über das Online Meldeportal <a href="http://www.ladv.de" target="blank">www.ladv.de</a> möglich. (Registrierung erforderlich)<br><br>
-  Für alle Läufe bitte Bestleistung 2012/13 angeben, um eine bestmögliche Laufbesetzung ermöglichen zu können.</td>
+  Für alle Läufe bitte Bestleistung 2013/14 angeben, um eine bestmögliche Laufbesetzung ermöglichen zu können.</td>
 </tr>
 <tr>
   <th>Organisations-Gebühren:</th>
-  <td>Männer u. Frauen jew. 6.- Euro, Jugend 5.- Euro, Schüler/innen 3.- Euro. Bei Nachmeldungen bis zum Veranstaltungstag wird ein Aufschlag von 2.- Euro erhoben.</td>
+  <td>Männer u. Frauen jew. 10.- Euro, Jugend 7.- Euro, Schüler/innen 5.- Euro. Bei Nachmeldungen bis zum 04. Juni 2014 wird ein Aufschlag von 10.- Euro erhoben.</td>
 </tr>
 <tr>
   <th>Meldeschluss:</th>
-  <td>Mittwoch, 29. Mai 2013 (Posteingang)<br />
-    Nachmeldungen nur in Ausnahmefällen gegen erhöhte Organisationsgebühr bis Freitag, 7. Juni 2013 möglich.<br />
+  <td>Mittwoch, 28. Mai 2014 (Posteingang)<br />
+    Nachmeldungen nur in Ausnahmefällen gegen erhöhte Organisationsgebühr bis Freitag, 4. Juni 2014 möglich.<br />
     <br />
     Aufgrund der Erfahrungen der letzten Jahre kann es zu einem vorzeitigen  Nachmeldeschluss kommen.</td>
 </tr>
 <tr>
   <th>Startunterlagen:</th>
-  <td>Ausgabe 08. Juni 2013 ab 9.00 Uhr vereinsweise gegen Entrichtung der Organisationsgebühren
-oberhalb der Tribüne des Sportzentrums der Universität.</td>
+  <td>Ausgabe 07. Juni 2014 ab 11.00 Uhr vereinsweise gegen Entrichtung der Organisationsgebühren
+oberhalb der Tribüne des Sportzentrums der Universität (Laufnacht ab 15.00 Uhr am 06. Juni 2014).</td>
 </tr>
 <tr>
   <th>Gala Programm:</th>
   <td><p><span style="font-style: italic">Männer:</span> <br />
-    Galaprogramm: 100 m, 200 m, 400 m, 800 m, 110 m H, 400m H, 4x100 m (DLV-Test), Weit<br />
-    Laufnacht: 1.500 m, 5.000 m, 3.000 m Hindernis<br />
-      Rahmenwettbewerb/Vorprogramm: 100 m, 200 m, 400 m, 800 m, 1.500 m, Weit, Hoch, Stabhoch<br />
+    Galaprogramm: 100 m, 200 m*, 400 m*, 800 m, 1.500m, 110 m H, 400m H*, 4x100 m (DLV-Test), Weit - *DLV-Einladungswettbewerbe<br />
+	Rahmenwettbewerb/Vorprogramm: 100 m, Weit, Hoch, Stabhoch. Mittel-/Langstrecken/Hindernis siehe <a href="http://www.lg-telis-finanz.de/2014_ausschreibungen/06-06_laufnacht.pdf">Laufnacht 06. Juni 2014</a><br />
         <br />
         <span style="font-style: italic">Frauen:</span><br />
-      Galaprogramm: 100 m, 200 m, 400 m, 800 m, 1500 m, 100 m H, 4x100 m (DLV-Test), Hoch, Stabhoch<br />
-      Laufnacht: 3.000 m Hindernis, 5000 m<br />
-      Rahmenwettbewerb/Vorprogramm: 100 m, 200 m, 400 m, 800 m, 1.500 m, Weit, Hoch<br />
+      Galaprogramm: 100 m, 200 m*, 400 m*, 800 m, 100 m H, 4x100 m (DLV-Test), Hoch, Stabhoch - *DLV Einladungswettbewerb<br />
+      Rahmenwettbewerb/Vorprogramm: 100 m, Weit, Hoch. Mittel-/Langstrecken/Hindernis siehe <a href="http://www.lg-telis-finanz.de/2014_ausschreibungen/06-06_laufnacht.pdf">Laufnacht 06. Juni 2014</a><br />
         <br />
         <span style="font-style: italic">männliche U20/U18:</span><br />
-      100 m, 200 m, 800 m, 1.500 m, Weit, Hoch<br />
+      100 m, Weit. Mittel-/Langstrecken/Hindernis siehe <a href="http://www.lg-telis-finanz.de/2014_ausschreibungen/06-06_laufnacht.pdf">Laufnacht 06. Juni 2014</a><br />
         <br />
         <span style="font-style: italic">weibliche U20/U18:</span><br />
-      100 m, 200 m, 800 m, 1.500 m, Weit, Hoch<br />
+      100 m, Weit, Hoch. Mittel-/Langstrecken/Hindernis siehe <a href="http://www.lg-telis-finanz.de/2014_ausschreibungen/06-06_laufnacht.pdf">Laufnacht 06. Juni 2014</a><br />
         <br />
         <span style="font-style: italic">Schüler U16:</span><br />
-      800 m, Weit<br />
+      Weit, 800m siehe <a href="http://www.lg-telis-finanz.de/2014_ausschreibungen/06-06_laufnacht.pdf">Laufnacht 06. Juni 2014</a><br />
         <br />
         <span style="font-style: italic">Schülerinnen U16:</span><br />
-      800 m<br />
+      Weit, 800 m siehe <a href="http://www.lg-telis-finanz.de/2014_ausschreibungen/06-06_laufnacht.pdf">Laufnacht 06. Juni 2014</a><br />
         <br />
         </p>
     </td>
 </tr>
 <tr>
-  <th>Bemerkung:</th>
-  <td><p>Die Zeitgrenzen für die Einteilung der A- und B-Läufe des Hauptprogramms werden nach
-Meldeschluss spätestens eine Woche vor dem 08. Juni bekannt gegeben. Gemeldete Teilnehmer, die
-diese Leistungen nicht erfüllen können, werden in die entsprechenden Wettbewerbe des
-Vorprogramms aufgenommen.<br />
-      <br />
-    Die 3000 m Hindernis der Männer/Junioren/Frauen/Juniorinnen finden als
-Bayerische Meisterschaften, national offen, statt.<br />
-    <br />
-   Stellplatzzeit bei allen Wettbewerben 75 Minuten vorher.<br />
-        <br />
-      Je 3 Athleten/-innen wird eine Betreuerkarte ausgegeben.</p>
-    </td>
+  <th>Zeitplan ab 29.04.2014 im Internet unter <a href="www.lg-regensburg.de">www.lg-regensburg.de</a></th>
 </tr>
 <tr>
   <th>Anfangshöhen:</th>
-  <td><span style="font-style: italic">Stabhoch Frauen:</span><br />
+  <td><span style="font-style: italic">Hoch Frauen:</span><br />
+    1,65 - 1,70 - 1,75 - 1,78 - 1,81 - 1,84 - dann 2cm jeweils höher<br>
+	<span style="font-style: italic">Hoch Mäner:</span><br />
+    1,85 - 1,90 - 1,95 - 2,00 - 2,05 - 2,09 - 2,13 - 2,16 - dann 2cm jeweils höher<br>
+	<span style="font-style: italic">Stabhoch Frauen:</span><br />
     3,60 m - 3,80 m - 4,00 m + 10 cm<br>
-    <span style="font-style: italic">Stabhoch Männer:</span><br />
-    4,40 m + 10 cm
     </td>
 </tr>
 <tr>
   <th>Ehrung:</th>
-  <td>Die ersten Acht erhalten Urkunden; die ersten Drei der Wettbewerbe des Hauptprogramms werden gesondert geehrt. </td>
+  <td>Die ersten Acht erhalten Urkunden; </td>
 </tr>
 <tr>
   <th>Infos:</th>
