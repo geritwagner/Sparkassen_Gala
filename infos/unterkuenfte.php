73b0f3f4a176c5b6794f8eee98a0c761bae9dde8
Gerit Wagner <gerit1989@googlemail.com>
2014-04-22 22:43:15 +0200

    Unterkünfte aktualisiert

--- infos/unterkuenfte.php
+++ infos/unterkuenfte.php
@@ -14,13 +14,10 @@ Furtmayrstr. 1, 93053 Regensburg, Tel +49 941/7804-0, Fax +49 941/7804-509</p>
 <p><span style="font-weight: bold"><a href="http://www.hotel-regensburg.bestwestern.de/" target="_blank">Hotel Best Western Premium</a></span><br />
 Ziegetsdorfer Straße 111, 93051 Regensburg, Tel +49 941/463930, Fax +49 941/46393400</p>
 
-<p><span style="font-weight: bold"><a href="http://www.db-training.de/site/dbtraining/de/service/hotel__gaestehaeuser/regensburg/regensburg.html">DB Gästehaus</a></span><br />
-Klosterackerweg 1, 93049 Regensburg, Tel +49 941-5003-13, Fax +49 941/5003-16</p>
-
 <p><span style="font-weight: bold"><a href="http://www.avia-hotel.de/start.html" target="_blank">AVIA Hotel</a></span><br />
 Frankenstraße 1-3, 93059 Regensburg, Tel +49 941/4098-0, Fax +49 941/4098-100</p>
 
-<p><span style="font-weight: bold"><a href="http://www.regensburghotel.de/st-georg/hotel-st-georg/herzlich-willkommen" target="_blank">Hotel St. Georg</a></span><br />
-Karl-Stieler-Str. 8, 93051 Regensburg, Tel +49 941 91090, Fax +49 941/948174</p>
+<p><span style="font-weight: bold"><a href="http://www.hansa-apart-hotel.de/" target="_blank">Hansa Apart Hotel</a></span><br />
+Friedenstraße 7, 93051 Regensburg, Tel +49 941 99290, Fax: +49 941 99 29 0 95, <a href="mailto:verkauf@hansa-apart-hotel.de">E-Mail</a></p>
 
 <?php require("../_footer.dir1.inc.php"); ?>
