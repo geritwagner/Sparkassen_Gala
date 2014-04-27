6d65b23762514f6b53a17286a93a4e19f2eac905
Gerit Wagner <gerit1989@googlemail.com>
2014-04-27 10:30:05 +0200

    Zeitplan hinzugefügt

--- 2014_gala/zeitplan.php
+++ 2014_gala/zeitplan.php
@@ -6,10 +6,362 @@
 <br>
 <p>Der Zeitplan ist ab dem 29.05.2014 auf dieser Seite verfügbar.</p>
 
-<!-- Stand 30.05.2013 - endgültiger Stand
+Stand 26.04.2014 <!-- endgültiger Stand -->
 <br /><br />
-<p>Der Zeitplan der Sparkassen Gala 2013 steht im PDF-Format zum Download/Drucken bereit: <a href="download/Sparkassen-Gala2013_Zeitplan.pdf" target="_blank">Zeitplan der Sparkassen Gala 2013 (26kb)</a></p>
+<p>Der Zeitplan der Sparkassen Gala 2014 steht im PDF-Format zum Download/Drucken bereit: <a href="download/Sparkassen-Gala2014_Zeitplan.pdf" target="_blank">Zeitplan der Sparkassen Gala 2014 (150kb)</a></p>
 </p>
-<br /> -->
+<br />
+<br />
+<center><p style="font-weight: bold">Laufnacht (Freitag, 6. Juni 2014)</p><center>
+
+
+  <table class="zeitplan" style="width:650px">
+    <tr>
+      <th width="10%">Zeit</th>
+      <th width="15%">Männer</th>
+      <th width="17%">mU20/U18</th>
+      <th width="13%">mU16</th>
+      <th width="15%">Frauen</th>
+      <th width="16%">wU20/U18</th>
+      <th width="14%">wU16</th>
+    </tr>
+	<TR VALIGN=TOP>
+		  <TD WIDTH=34 >17.30</TD>
+		  <TD>&nbsp;</TD>
+		  <TD>&nbsp;</TD>
+		  <TD>800m (2)</TD>
+		  <TD>&nbsp;</TD>
+		  <TD>&nbsp;</TD>
+		  <TD>800m (1)</TD>
+	</TR>
+	<TR VALIGN=TOP>
+		  <TD WIDTH=34 >18.00</TD>
+		  <TD>&nbsp;</TD>
+		  <TD>&nbsp;</TD>
+		  <TD>&nbsp;</TD>
+		  <TD>800m ZEL (<2:09)</TD>
+		  <TD>800m ZEL (<2:09)</TD>
+		  <TD>&nbsp;</TD>
+	</TR>
+	<TR VALIGN=TOP>
+		  <TD WIDTH=34 >18.45</TD>
+		  <TD>800m ZEL (<1:51)</TD>
+		  <TD>800m ZEL (<1:51)</TD>
+		  <TD>&nbsp;</TD>
+		  <TD>&nbsp;</TD>
+		  <TD>&nbsp;</TD>
+		  <TD>&nbsp;</TD>
+	</TR>
+	<TR VALIGN=TOP>
+		  <TD WIDTH=34 >19.30</TD>
+		  <TD>&nbsp;</TD>
+		  <TD>&nbsp;</TD>
+		  <TD>&nbsp;</TD>
+		  <TD>3000mH A-L</TD>
+		  <TD>&nbsp;</TD>
+		  <TD>&nbsp;</TD>
+	</TR>
+	<TR VALIGN=TOP>
+		  <TD WIDTH=34 >19.55</TD>
+		  <TD>&nbsp;</TD>
+		  <TD>&nbsp;</TD>
+		  <TD>&nbsp;</TD>
+		  <TD>1500m ZEL (<4:30)</TD>
+		  <TD>1500m ZEL (<4:30)</TD>
+		  <TD>&nbsp;</TD>
+	</TR>
+	<TR VALIGN=TOP>
+		  <TD WIDTH=34 >20.10</TD>
+		  <TD>1500m ZEL (<3:51)</TD>
+		  <TD>1500m ZEL (<3:51)</TD>
+		  <TD>&nbsp;</TD>
+		  <TD>&nbsp;</TD>
+		  <TD>&nbsp;</TD>
+		  <TD>&nbsp;</TD>
+	</TR>
+	<TR VALIGN=TOP>
+		  <TD WIDTH=34 >21.00</TD>
+		  <TD>3000mH EL</TD>
+		  <TD>&nbsp;</TD>
+		  <TD>&nbsp;</TD>
+		  <TD>&nbsp;</TD>
+		  <TD>&nbsp;</TD>
+		  <TD>&nbsp;</TD>
+	</TR>
+	<TR VALIGN=TOP>
+		  <TD WIDTH=34 >21.35</TD>
+		  <TD>5000m (B-Lauf)</TD>
+		  <TD>&nbsp;</TD>
+		  <TD>&nbsp;</TD>
+		  <TD>5000m</TD>
+		  <TD>&nbsp;</TD>
+		  <TD>&nbsp;</TD>
+	</TR>
+</table>
+<br />
+<br />
+<center><p style="font-weight: bold">Vorprogramm Sparkassen Gala (Samstag, 7. Juni 2014))</p><center>
+
+  <table class="zeitplan" style="width:650px">
+    <tr>
+      <th width="10%">Zeit</th>
+      <th width="15%">Männer</th>
+      <th width="17%">mU20/U18</th>
+      <th width="13%">mU16</th>
+      <th width="15%">Frauen</th>
+      <th width="16%">wU20/U18</th>
+      <th width="14%">wU16</th>
+    </tr>
+	<TR VALIGN=TOP>
+		<TD WIDTH=34 >12.30</TD>
+		<TD>Hoch E</TD>
+		<TD>Hoch E</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+	</TR>
+	<TR VALIGN=TOP>
+		<TD WIDTH=34 >12.30</TD>
+		<TD>100m ZEL</TD>
+		<TD>100m ZEL</TD>
+		<TD>Weit VE (NP)</TD>
+		<TD>Stabhoch E</TD>
+		<TD>Weit</TD>
+		<TD>&nbsp;</TD>
+	</TR>
+	<TR VALIGN=TOP>
+		<TD WIDTH=34 >13.00</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+	</TR>
+	<TR VALIGN=TOP>
+		<TD WIDTH=34 >13.15</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>100m ZEL</TD>
+		<TD>100m ZEL</TD>
+		<TD>&nbsp;</TD>
+	</TR>
+		<TR VALIGN=TOP>
+		<TD WIDTH=34 >13.45</TD>
+		<TD>&nbsp;</TD>
+		<TD>400m DLV U20</TD>
+		<TD>&nbsp;</TD>
+		<TD>Hoch E</TD>
+		<TD>Hoch E</TD>
+		<TD>&nbsp;</TD>
+	</TR>
+		<TR VALIGN=TOP>
+		<TD WIDTH=34 >14.00</TD>
+		<TD>Weit B-W</TD>
+		<TD>Weit mU20 VE (NP)</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+	</TR>
+		<TR VALIGN=TOP>
+		<TD WIDTH=34 >14.10</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>100m VL (A-D)</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+	</TR>
+		<TR VALIGN=TOP>
+		<TD WIDTH=34 >14.35</TD>
+		<TD>100m VL (A-E)</TD>
+		<TD>DLV innerh. A-E</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+	</TR>
+	<TR VALIGN=TOP>
+		<TD WIDTH=34 >15.00</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>100m Hü VL(2)</TD>
+		<TD>100m Hü VL(1)</TD>
+		<TD>&nbsp;</TD>
+	</TR>
+	<TR VALIGN=TOP>
+		<TD WIDTH=34 >15.15</TD>
+		<TD>110m Hü VL(2)</TD>
+		<TD>110m Hü VL*(1)</TD>
+		<TD>&nbsp;</TD>
+		<TD style="background-color: 	#C0C0FF">Hoch E</TD>
+		<TD>Hoch E</TD>
+		<TD>&nbsp;</TD>
+	</TR>
+</table>
+<br />
+<br />
+<p><span style="font-weight: bold">Gala-Hauptprogramm (Samstag, 7. Juni 2014)</span></p>
+<br />
+<table class="zeitplan" style="width:650px">
+    <tr>
+      <th width="10%">Zeit</th>
+      <th width="15%">Männer</th>
+      <th width="17%">mU20/U18</th>
+      <th width="13%">mU16</th>
+      <th width="15%">Frauen</th>
+      <th width="16%">wU20/U18</th>
+      <th width="14%">wU16</th>
+    </tr>
+
+	<TR>
+		<TD WIDTH=34 >16.00</TD>
+		<TD style="background-color: 	#C0C0FF">Stabhoch E</TD>
+		<TD>Weit mU18 VE (NP)</TD>
+		<TD>&nbsp;</TD>
+		<TD style="background-color: 	#C0C0FF">100m Hü EL(2)</TD>
+		<TD>100m Hü EL(1)</TD>
+		<TD WIDTH=65>&nbsp;</TD>
+	</TR>
+	<TR>
+		<TD WIDTH=34 >16.15</TD>
+		<TD style="background-color: 	#C0C0FF">110m Hü EL(2)</TD>
+		<TD>110m Hü EL(1)</TD>
+		<TD>&nbsp;</TD>
+		<TD>100m Hü EL(2)</TD>
+		<TD>100m Hü EL(1)</TD>
+		<TD WIDTH=65>&nbsp;</TD>
+	</TR>
+	<TR>
+		<TD WIDTH=34 >16.30</TD>
+		<TD>Weit VE</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD style="background-color: 	#C0C0FF">100m EL(2)</TD>
+		<TD>&nbsp;</TD>
+		<TD WIDTH=65>&nbsp;</TD>
+	</TR>
+	<TR>
+		<TD WIDTH=34 >16.45</TD>
+		<TD style="background-color: 	#C0C0FF">100m EL(2)</TD>
+		<TD>100m EL(1)</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD WIDTH=65>&nbsp;</TD>
+	</TR>
+		<TR>
+		<TD WIDTH=34 >17.00</TD>
+		<TD style="background-color: 	#C0C0FF">400m Hü EL (A+B)</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD WIDTH=65>&nbsp;</TD>
+	</TR>
+		<TR>
+		<TD WIDTH=34 >17.20</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>400m Hü DLV</TD>
+		<TD>&nbsp;</TD>
+		<TD WIDTH=65>&nbsp;</TD>
+	</TR>
+		<TR>
+		<TD WIDTH=34 >17.30</TD>
+		<TD style="background-color: 	#C0C0FF">400m (A+B)</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>400m Hü DLV</TD>
+		<TD>&nbsp;</TD>
+		<TD WIDTH=65>&nbsp;</TD>
+	</TR>
+		<TR>
+		<TD WIDTH=34 >17.50</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD style="background-color: 	#C0C0FF">800m ZEL (A+B)</TD>
+		<TD>&nbsp;</TD>
+		<TD WIDTH=65>&nbsp;</TD>
+	</TR>
+		<TR>
+		<TD WIDTH=34 >18.05</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>4x100m ZEL</TD>
+		<TD WIDTH=65>Weit wU18 VE (NP)</TD>
+		<TD>&nbsp;</TD>
+	</TR>
+	<TR>
+		<TD WIDTH=34 >18.20</TD>
+		<TD>4x100m ZEL</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD WIDTH=65>&nbsp;</TD>
+	</TR>
+	<TR>
+		<TD WIDTH=34 >18.35</TD>
+		<TD style="background-color: 	#C0C0FF">800m ZEL(A-B(</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>Weit VE</TD>
+		<TD>&nbsp;</TD>
+		<TD WIDTH=65>&nbsp;</TD>
+	</TR>
+	<TR>
+		<TD WIDTH=34 >18.50</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD style="background-color: 	#C0C0FF">200m ZEL(A+B)</TD>
+		<TD>&nbsp;</TD>
+		<TD WIDTH=65>&nbsp;</TD>
+	</TR>
+	<TR>
+		<TD WIDTH=34 >19.05</TD>
+		<TD style="background-color: 	#C0C0FF">200m ZEL (A+B)(2)</TD>
+		<TD>200m E DLV</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD WIDTH=65>&nbsp;</TD>
+	</TR>
+	<TR>
+		<TD WIDTH=34 >19.25</TD>
+		<TD style="background-color: 	#C0C0FF">1500m A+B</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD WIDTH=65>&nbsp;</TD>
+	</TR>
+	<TR>
+		<TD WIDTH=34 >19.45</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD>&nbsp;</TD>
+		<TD style="background-color: 	#C0C0FF">1500m A-Lauf</TD>
+		<TD>&nbsp;</TD>
+		<TD WIDTH=65>&nbsp;</TD>
+	</TR>
+</TABLE>
+<br>
+<p style="font-weight: bold">VL= Vorlauf, ZVL=Zeitvorlauf, EL=Endlauf, ZEL=Zeitendläufe, E=Endkampf, VE=Vor/Endkampf</p>
+</center>
+<p align="justify" style="font-size:0.8em">Die ersten Vier über 800m/1500m  der Laufnacht qualifizieren sich für die A/B-Läufe der Sparkassen Gala am Samstag</p>
+<p  style="background-color: 	#C0C0FF">
+Blau markierte Disziplinen sind Gala-Wettbewerbe (mit Preisgeld)
+</p>
+<br />
+
 
 <?php require("_footer.inc.php"); ?>
