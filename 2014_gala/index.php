4a68677f8f70ef1ff34003fe1830a5f09e277842
Gerit Wagner <gerit1989@googlemail.com>
2014-05-09 16:59:42 +0200

    Trailer: Autoplay und Größe geändert

--- 2014_gala/index.php
+++ 2014_gala/index.php
@@ -2,10 +2,8 @@
  $menu['current'] = "aktuelles"; ?>
 <?php require("_header.inc.php"); ?>
 
-<h2>Trailer zur Sparkassen Gala 2014</h2>
-<br />
-<p align="center">
-<embed src="http://videos.lg-telis-finanz.de/2013/sparkassen-gala/player.swf" bgcolor="000000" allowfullscreen="true" allowscriptaccess="always" flashvars="file=http://videos.lg-telis-finanz.de/2014/galatrailer2014_final.mp4&amp;image=http://videos.lg-telis-finanz.de/2014/galatrailer2014_final.jpg&amp;stretching=exactfit&amp;plugins=embed-1" height="290" width="480"> <br></p>
+<p>
+<embed src="http://videos.lg-telis-finanz.de/2013/sparkassen-gala/player.swf" bgcolor="000000" allowfullscreen="true" allowscriptaccess="always" flashvars="file=http://videos.lg-telis-finanz.de/2014/galatrailer2014_final.mp4&amp;image=http://videos.lg-telis-finanz.de/2014/galatrailer2014_final.jpg&amp;stretching=exactfit&amp;plugins=embed-1&controls=false&autostart=true" height="363" width="600"> <br></p>
 
 <h2>Die Sparkassen Gala 2014 verspricht wieder Großes</h2>
 
@@ -13,8 +11,4 @@
 
 <p><img src="../2012/impressionen/kiefner/16.JPG" class="float_left" width="240px">Regensburg, 10. Mai 2014 (orv) –  An Tagen wie diesen, da kann so manches passieren. Dieser Satz gilt uneingeschränkt auch in diesem Jahr für die Sparkassen Gala, die heuer am 6./7. Juni wieder im Regensburger Uni-Stadion stattfindet. Deutschland braucht solche Meetings, einerseits für den Nachwuchs, die im Vorprogramm, aber auch in der „Laufnacht“ - in diesem Jahr erstmals am Tag zuvor platziert - ihr Können bei entsprechender Konkurrenz zeigen können, andererseits für viele deutsche Asse, die noch nicht den Zugang in die internationalen Topevents finden. Der Sprint, die Kurzhürden, die Kurzstaffeln, die Mittelstrecken mit den local heroes Corinna Harrer und Florian Orth, aber zusehends auch die Stadionrunde mit oder ohne Hürden bringen Jahr für Jahr spannende Wettkämpfe und tolle Leistungen hervor. Der Stern eines Adam Gemili, der in Regensburg eine 10,08 auf die Bahn zauberte, ist hier aufgegangen. Inzwischen ist der Bursche auch die 200m unter 20 Sekunden gelaufen. <a href="/news/09-05_vorschau.php">Hier</a> geht's weiter ...</p>
 
-<h2>Seite der Sparkassen Gala 2014 ist online</h2>
-<br />
-<p>Regensburg, 27. März 2014 - Ab sofort finden Sie an dieser Stelle alle Informationen zur Sparkassen Gala 2014 am 07. Juni im Regensburger Uni Stadion. <!-- <a href="ausschreibung.php">Hier</a> geht's zur Ausschreibung. -->
-</p>
 <?php require("_footer.inc.php"); ?>
