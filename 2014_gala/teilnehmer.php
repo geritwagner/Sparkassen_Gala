a4bae6bb45624816c1cf84e4919febba40e48e37
Gerit Wagner <gerit1989@googlemail.com>
2014-06-03 11:30:58 +0200

    Updates Zeitplan, Teilnehmer und Artikel

--- 2014_gala/teilnehmer.php
+++ 2014_gala/teilnehmer.php
@@ -7,7 +7,7 @@
 <!-- <p>Die Teilnehmerliste wird im Vorfeld der Sparkassen-Gala auf dieser Seite veröffentlicht.</p> -->
 
 <p>
-<a href="/download/Sparkassen_Gala_2014_Teilnehmerliste.htm" target="_blank">&raquo; Teilnehmerliste Stand 30.05.2014</a>
+<a href="/download/Sparkassen_Gala_2014_Teilnehmerliste.htm" target="_blank">&raquo; Teilnehmerliste Stand 02.06.2014</a>
 <!-- <br />Letzter Stand vor der Veranstaltung, kleinere Änderungen sind nur noch am Wettkampftag nach Rücksprache mit der Wettkampfleitung möglich. --></p> 
 
 <?php require("_footer.inc.php"); ?>
