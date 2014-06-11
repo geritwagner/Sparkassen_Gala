98e408532560884e562812e8aa2832f43ee0cc00
Gerit Wagner <gerit1989@googlemail.com>
2014-06-11 18:09:25 +0200

    Youtube Link

--- 2014_gala/index.php
+++ 2014_gala/index.php
@@ -2,6 +2,14 @@
  $menu['current'] = "aktuelles"; ?>
 <?php require("_header.inc.php"); ?>
 
+
+<h2>Die Videos der Sparkassen Gala sind online</h2>
+<br />
+<iframe width="560" height="315" src="//www.youtube.com/embed/videoseries?list=PLKSOlqrE36tm9U-OUsGeiCt8aKh4M3KaO" frameborder="0" allowfullscreen></iframe>
+<br>
+<br>
+
+
 <h2>Die Sparkassen Gala noch einmal zum Nachlesen</h2>
 <br />
 <p>Regensburg, 07. Juni 2014 - Den Tag der Sparkassen Gala noch einmal zum Nachlesen finden Sie hier im <a href="http://www.live.mittelbayerische.de/Event/Gipfeltreffen_der_Leichtathleten" target="_blank">&raquo; Blog</a></p>
