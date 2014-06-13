6a0d2aa50a94b13919bd6b44a1a199eafe455aee
Gerit Wagner <gerit1989@googlemail.com>
2014-06-13 14:49:58 +0200

    Korrektur Ergebnisliste

--- 2014_gala/ergebnisse.php
+++ 2014_gala/ergebnisse.php
@@ -10,8 +10,9 @@
 
 Die Ergebnisliste der Sparkassen Gala 2014 finden Sie hier <a href="download/Sparkassen_Gala_2014_Ergebnisse.html" target="_blank">&raquo; Gesamtergebnisliste 2014</a></p>
 <!--  <a href="http://www.sparkassen-gala.de/download/Sparkassen_Gala_2012_Ergebnisse.htm" target="_blank">Hier</a> geht's zur Gesamtergebnisliste. -->
-<br /><br />
+<!-- <br /><br />
 Die <a href="http://live.seltec-sports.de/2014/regensburg/" target="_blank">&raquo; Live-Ergebnisse</a> der Sparkassen Gala 2014 finden Sie hier:</p>
+
 
 <iframe src="http://live.seltec-sports.de/2014/regensburg/" width="100%" height="2000" name="Live Ergebnisse" scrolling="no" marginheight="0" marginwidth="0" frameborder="0">
   <p>Ihr Browser kann leider keine eingebetteten Frames anzeigen:
