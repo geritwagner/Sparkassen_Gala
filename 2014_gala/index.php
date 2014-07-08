3a8f37bba73a6f3377563b255004a86778abe1cd
Gerit Wagner <gerit1989@googlemail.com>
2014-07-08 18:23:04 +0200

    Index Aufgeräumt

--- 2014_gala/index.php
+++ 2014_gala/index.php
@@ -5,22 +5,18 @@
 
 <h2>Die Videos der Sparkassen Gala sind online</h2>
 <br />
-
 <p>Die <a href="https://www.youtube.com/playlist?list=PLKSOlqrE36tm9U-OUsGeiCt8aKh4M3KaO" target="_blank">Übersicht der einzelnen Disziplinen</a> finden Sie in der Playlist.</p>
 <iframe width="560" height="315" src="//www.youtube.com/embed/videoseries?list=PLKSOlqrE36tm9U-OUsGeiCt8aKh4M3KaO" frameborder="0" allowfullscreen></iframe>
 <br>
 <br>
 
-
 <h2>Die Sparkassen Gala noch einmal zum Nachlesen</h2>
 <br />
 <p>Regensburg, 07. Juni 2014 - Den Tag der Sparkassen Gala noch einmal zum Nachlesen finden Sie hier im <a href="http://www.live.mittelbayerische.de/Event/Gipfeltreffen_der_Leichtathleten" target="_blank">&raquo; Blog</a></p>
 
-
 <h2>Christiane Klopsch zur Bestzeit</h2>
 <br />
 <p><img class="float_left" src="http://sparkassen-gala.de/photos/kiefner/22.JPG"  width="150" />07. Juni 2014 (leichtathletik.de) - Die 800 Meter kontrollierte mit Fabienne Kohlmann (LG Karlstadt-Gambach-Lohr) die Favoritin. Sie steigerte ihre Saisonbestzeit auf 2:01,70 Minuten. "Ich wäre gerne schneller gelaufen. Es fehlt noch ein bisschen das richtige Rennen. Ich muss mich auch noch richtig einschätzen können", sagte sie. Die Regensburgerin Corinna Harrer (2:02.50 min) kämpfte sich auf den zweiten Platz: "Nach den 5.000 Metern von Koblenz waren die 800 wie ein Sprint." Silvio Schirrmeister konnte sich auf den 400 Meter Hürden einmal mehr als bester Deutscher positionieren. Der Chemnitzer war mit seiner Zeit von 49,85 Sekunden aber nicht ganz zufrieden. Schnellster aller Läufe war der Schweizer Kariem Hussein (49,33 sec). Bei den Frauen durfte sich Christiane Klopsch über eine neue Bestzeit von 56,02 Sekunden freuen. Die Athletin der LG OVAG Friedberg-Fauerbach war damit an der Donau ungefährdet. Auch die EM-Norm (55,90 sec) ist in Reichweite. "Der Lauf war nicht perfekt. Aber es ist gut, wenn man Bestzeit läuft und man weiß, es geht noch um einiges besser", sagte Christiane Klopsch.</p>
-
 
 <h2>Kamghe Gaba eilt zur EM-Norm</h2>
 <br />
@@ -41,7 +37,6 @@
 <p><img class="float_left" src="http://sparkassen-gala.de/photos/Kiefner/Julian_Reus_KiefnerFoto.jpg"  width="160" />07. Juni 2014 (leichtathletik.de) - Der Deutsche Meister Julian Reus nutzte die Vorläufe über 100 Meter, um gleich eine Zeit von 10,12 Sekunden auf Bahn zu zaubern und sich damit in der deutschen Jahresbestenliste auf Platz zwei zu verbessern. Zugleich war es die drittschnellste Zeit des Wattenscheiders in seiner Karriere. U20-Europameister Tobias Potye bestätigte im Hochsprung die 2,22 Meter, die er schon vor einer Woche in Sinn gesprungen war. "Ich bin ruhig geblieben und habe mich auf meine Sprünge konzentriert", erklärte der 19-Jährige sein Erfolgsrezept. Der Athlet vom FC Aschheim ist damit auf dem besten Weg zur U20-Weltmeisterschaft in Eugene (USA).</p>
 <br><br>
 
-
 <p>
 <video id="player_a" class="projekktor" poster="intro.png" title="Gala Trailer" width="640" height="360" controls>
     <source src="http://videos.lg-telis-finanz.de/2014/galatrailer2014_final.mp4" type="video/mp4" />
@@ -49,14 +44,9 @@
 <!-- 
 <embed src="http://videos.lg-telis-finanz.de/2013/sparkassen-gala/player.swf" bgcolor="000000" allowfullscreen="true" allowscriptaccess="always" flashvars="file=http://videos.lg-telis-finanz.de/2014/galatrailer2014_final.mp4&amp;image=http://videos.lg-telis-finanz.de/2014/galatrailer2014_final.jpg&amp;stretching=exactfit&amp;plugins=embed-1&controls=false&autostart=false" height="363" width="600"> --> <br><br></p>
 
-
-<h2>Live-Stream ab 15.45 Uhr</h2>
+<!-- <h2>Live-Stream ab 15.45 Uhr</h2>
 <br />
-<p>Auch für die Sparkassen Gala 2014 ist wieder ein Live-Stream geplant. Der Stream startet zum Hauptprogramm um 15.45 Uhr.</p>
-
-<br>
-
-
+<p>Auch für die Sparkassen Gala 2014 ist wieder ein Live-Stream geplant. Der Stream startet zum Hauptprogramm um 15.45 Uhr.</p> -->
 
 <h2>Sperrung der A3 Ausfahrt Regensburg Universität in Fahrtrichtung Passau</h2>
 <br />
@@ -64,26 +54,18 @@
 <br /><a target="_blank" href="https://goo.gl/maps/1Z87A">&raquo; Karte</a>
 </p>
 
-
 <h2>Plötzlich ist im Weitsprung der Teufel los</h2>
-
 <p><strong>Absage von Wesel besorgt Sparkassen Gala einen Weitsprung-Boom</strong></p>
-
 <p><img class="float_left" title="Mittelstrecke in action (Kosmider-Foto)" src="http://lg-telis-finanz.de/images/stories/2012/ZieglerManuel_BLVU23U16_Kiefnerfoto.jpg" alt="Ziegler (Kiefnerfoto)" width="130" />Regensburg, 6. Juni 2014 (orv) –  Es war nur eine dürre Meldung, die so mancher übersah. Die Regensburger von der LG Telis Finanz waren da schon näher am Ball. Ihr Top-Dreispringer Manuel Ziegler kann bei der Team-EM-Ausscheidung in Wesel nicht springen, weil das Meeting vom DLV aus hier nicht näher erläuterten Gründen plötzlich keine Genehmigung erfuhr. Der Dreisprung hängt noch immer in der Luft, für den Weitsprung hat sich eine Alternative gefunden, die Sparkassen Gala in Regensburg. So können am Samstag nun neben der derzeit Führenden in der Deutschen Bestenliste Lena Malkus (6,88m) und Lokalmatadorin Michelle Weitzel (6,66m) gleich mit Malaika Mihambo (6,81m), Nadja Käther (6,57m), Marysa Luzolo (6,45m), Xenia Achkinaze (6,44m) und Lisa Kurschilgen (6,42m) fünf weitere Top-Springerinnen bewundert werden. Bei den Männern hat es den 8,71m-Hallen-Europarekordler Sebastian Bayer hereingeschneit, der auch schon im Freien 8,49m gesprungen ist. <a href="/news/06-06_Weitsprung.php">Hier</a> geht's weiter ...</p>
- 
  
 
 <h2>Spezialistinnen gegen Allrounderinnen</h2>
-
 <p><strong>800m Frauenrennen bei der Sparkassen Gala mit Brisanz</strong></p>
-
 <p><img class="float_left" title="Mittelstrecke in action (Kosmider-Foto)" src="http://www.lg-telis-finanz.de/images/stories/2013/Harrer7_DM2013_Kosmiderfoto.jpg" alt="Harrer7 DM2013 Kosmiderfoto" width="197" />Regensburg, 04. Juni 2014 (orv) –&nbsp; Die 800m der Frauen waren bei der Sparkassen Gala von jeher ein Highlight. Schließlich ist es das Wohnzimmer der Corinna Harrer. Jener Läuferin, die in den letzten Jahren mit 2:00,34 Minuten die schnellste deutsche Zeit erzielt hat. Sozusagen im Vorübergehen, weil die 800m nicht unbedingt ihre Spezialstrecke sind. Das heißt aber nicht, dass sie auf den zwei Stadionrunden nicht auch schon erfolgreich gewesen wäre. Der zweite Platz bei der U20-EM 2009 in Novisad und zwei nationale Titel im Nachwuchsbereich können sich durchaus sehen lassen. Ansonsten sammelt die Regensburgerin auf vielen Strecken Titel genauso wie ihre kaum schwächeren Teamkolleginnen Maren Kock und Thea Heim. Die können inzwischen auch passabel 800m laufen, was Thea Heim eindrucksvoll letzte Woche mit ihren von niemand erwarteten 2:03,20min bewiesen hat. <a href="/news/04-06_Spezialistinnen gegen Allrounderinnen.php">Hier</a> geht's weiter ...</p>
  
  
 <h2>Die Sparkassen Gala 2014 wird wieder ein Leichtathletik-Festival</h2>
-
 <p><strong>Opening bereits am Freitag, ab 17.30 Uhr mit der Laufnacht</strong></p>
-
 <p><img class="float_left" src="http://www.lg-telis-finanz.de//images/stories/2013/Harrer_c_dm12_foto_kiefner.jpg" alt="Harrer c dm12 foto kiefner" width="130" />Regensburg, 02. Juni 2014 (orv) –&nbsp; Alle Jahre wieder … kann man Anfang Juni im Regensburger Uni-Stadion absolute Top-Leichtathletik bewundern. In diesem Jahr findet die Sparkassen Gala der LG Telis Finanz Regensburg am kommenden Samstag, ab 16 Uhr statt. Neu dabei ist, dass die Langstrecken mit den Hindernisentscheidungen und den offenen Mittelstreckenrennen bereits am Freitag, ab 17.30 Uhr vorgeschaltet werden. Am Gala-Tag selbst stimmt ein breitgefächertes Vorprogramm für alle Leichtathleten/Innen, die später auch einmal ganz nach oben wollen statt. In der Sparkassen Gala selbst können die Zuschauer und Fans der olympischen Kernsportart Nummer eins wieder ein Feuerwerk von Highlights erwarten. <a href="/news/03-06_leichtathletik-festival.php">Hier</a> geht's weiter ...</p>
  
  
