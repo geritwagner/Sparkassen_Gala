84d9cad7d5b575646d5681cd0821466a53b7be37
geritwagner <gerit.wagner@stud.uni-regensburg.de>
2015-06-06 18:01:24 +0200

    livestream-popup (latin charset)

--- 2015_gala/livestream-popup.php
+++ 2015_gala/livestream-popup.php
@@ -40,7 +40,7 @@ h1 { font-size: 1.7em; color: #E2001A; padding:0; margin: 0; }
 
     <br />
 
-<div id="footnote">Ein Angebot der <a href="http://www.lg-telis-finanz.de/" target="_blank">LG TELIS FINANZ Regensburg</a>, produziert von <a href="http://www.seltec.at/" target="_blank">Seltec</a>,
+<div id="footnote">Es gibt leider kleine Probleme mit dem Live Stream, wir bitten die Störungen zu entschuldigen und arbeiten an der Lösung.<br /><br />Ein Angebot der <a href="http://www.lg-telis-finanz.de/" target="_blank">LG TELIS FINANZ Regensburg</a>, produziert von <a href="http://www.seltec.at/" target="_blank">Seltec</a>,
 unterst&uuml;tzt durch die <a href="http://www.telis-finanz.de/" target="_blank">TELIS FINANZ AG</a>,
 weitere Infos unter <a href="http://www.sparkassen-gala.de/" target="_blank">www.sparkassen-gala.de</a> - Feedback bitte an <script language="JavaScript">
 document.write('<a hre'+'f="ma'+'ilto'+':'+'%76%69%64%65%6f%73%40%6c%67%2d%74%65%6c%69%73%2d%66%69%6e%61%6e%7a%2e%64%65">&#118;&#105;&#100;&#101;&#111;&#115;&#64;&#108;&#103;&#45;&#116;&#101;&#108;&#105;&#115;&#45;&#102;&#105;&#110;&#97;&#110;&#122;&#46;&#100;&#101;<\/a>');
