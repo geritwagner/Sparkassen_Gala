16b014b5ed0054b94f41ab8d5481f37d8ada1805
Gerit Wagner <gerit.wagner@wiwi.uni-regensburg.de>
2016-04-15 20:46:49 +0200

    zeitplan

--- 2016_gala/zeitplan.php
+++ 2016_gala/zeitplan.php
@@ -4,6 +4,874 @@
 
 	<h2>Zeitplan</h2>
 	<br>
-	<p>Der Zeitplan wird im Vorfeld der Sparkassen-Gala auf dieser Seite veröffentlicht.</p>
+  <h2>Zeitplan</h2>
+<!--  <br /> Den Zeitplan gibt es auch zum Download und Ausdrucken <a href="javascript:void(0)" class="download-zeitplan" target="_blank">» Zeitplan.pdf</a> -->
+  <br />
+  <br />
+  <p style="text-align: center;">
+    <strong>
+      Regensburger Laufnacht und Sparkassen-Leichtathletik Gala 2016
+    </strong>
+  </p>
+  <p style="text-align: center;">
+    <strong>
+      Laufnacht (Samstag, 4. Juni 2016)
+    </strong>
+  </p>
+  <table class="zeitplan" style="width:650px">
+    <tr>
+      <td>
+        Zeit
+      </td>
+      <td>
+        Männer
+      </td>
+      <td>
+        mU20/18
+      </td>
+      <td>
+        mU16
+      </td>
+      <td>
+        Frauen
+      </td>
+      <td>
+        wU20/18
+      </td>
+      <td>
+        wU16
+      </td>
+    </tr>
+    <tr>
+      <td>
+        13.15
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+        200m offen
+      </td>
+      <td>
+        200m offen
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        13.45
+      </td>
+      <td>
+        200m offen
+      </td>
+      <td>
+        200m offen
+      </td>
+      <td>
+        Weit VE (NP)
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        14.30
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+        400m D-F
+      </td>
+      <td>
+        400m DLV U20
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        15.00
+      </td>
+      <td>
+        400m D-E
+      </td>
+      <td>
+        400m DLV U20
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        15.50
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+        400m A-C
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        16.10
+      </td>
+      <td>
+        400m A-C
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        16.30
+      </td>
+      <td>
+        400m Hü
+      </td>
+      <td>
+        400m Hü
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+        Weit  wU20 VE
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        17.00
+      </td>
+      <td>
+        400m Hü
+      </td>
+      <td>
+        400m Hü
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        17.30
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+        800m (2)
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+        800m (1)
+      </td>
+    </tr>
+    <tr>
+      <td>
+        18.00
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+        800 m ZEL
+      </td>
+      <td>
+        800 m ZEL
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        18.20
+      </td>
+      <td>
+        800 m ZEL
+      </td>
+      <td>
+        800 m ZEL
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        19.00
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+        3000 m Hi EL
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        19.20
+      </td>
+      <td>
+        Weit B-W.
+      </td>
+      <td>
+        Weit mU20 VE (NP)
+      </td>
+      <td>
+      </td>
+      <td>
+        1500 m ZEL
+      </td>
+      <td>
+        1500 m ZEL
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        19.45
+      </td>
+      <td>
+        1500 m ZEL
+      </td>
+      <td>
+        1500 m ZEL
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        20.15
+      </td>
+      <td>
+        3000 m Hi EL
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+        Weit wU18 VE (NP)
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        20.35
+      </td>
+      <td>
+        5000 m
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        21.00
+      </td>
+      <td>
+        5000 m
+      </td>
+      <td>
+      </td>
+      <td>
+        5000 m
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td colspan="7" height="80px" style="vertical-align:middle; text-align:center;">
+        <strong>
+          Vorprogramm (Sonntag, 6. Juni 2016)
+        </strong>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        Zeit
+      </td>
+      <td>
+        Männer
+      </td>
+      <td>
+        mU20/18
+      </td>
+      <td>
+        mU16
+      </td>
+      <td>
+        Frauen
+      </td>
+      <td>
+        wU20/18
+      </td>
+      <td>
+        wU16
+      </td>
+    </tr>
+    <tr>
+      <td>
+        11.15
+      </td>
+      <td>
+        Hoch E
+      </td>
+      <td>
+        Hoch E
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        11.15
+      </td>
+      <td>
+        100 m ZEL
+      </td>
+      <td>
+        100 m ZEL
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        12.00
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+        100 m ZEL
+      </td>
+      <td>
+        100 m ZEL
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        12.30
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+        100 m Hü VL(2)
+      </td>
+      <td>
+        100 m Hü VL(1)
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        12.45
+      </td>
+      <td>
+        110 m Hü VL(2)
+      </td>
+      <td>
+        110 m Hü VL*(1)
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+        Hoch E
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        13.15
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+        100 m VL (A-C)
+      </td>
+      <td>
+        DLV A-B VL
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        13-30
+      </td>
+      <td>
+        100 m VL (A-C)
+      </td>
+      <td>
+        DLV A-B VL
+      </td>
+      <td>
+      </td>
+      <td>
+        Hoch E
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td colspan="7" height="80px" style="vertical-align:middle; text-align:center;">
+        <strong>
+          Gala-Programm (Samstag, 6. Juni 2016)
+        </strong>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        Zeit
+      </td>
+      <td>
+        Männer
+      </td>
+      <td>
+        mU20/18
+      </td>
+      <td>
+        mU16
+      </td>
+      <td>
+        Frauen
+      </td>
+      <td>
+        wU20
+      </td>
+      <td>
+        wU16
+      </td>
+    </tr>
+    <tr>
+      <td>
+        14.00
+      </td>
+      <td>
+        110 m Hü EL(1)
+      </td>
+      <td>
+        Weit mU20 DLV VE
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        14.15
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+        100 m Hü EL(2)
+      </td>
+      <td>
+        100 m Hü E (1)
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        14.30
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+        100 m EL (2)
+      </td>
+      <td>
+        100 m E (1)
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        14.45
+      </td>
+      <td>
+        100 m EL (2)
+      </td>
+      <td>
+        100 m EL(1)
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        15.00
+      </td>
+      <td>
+        800 m ZEL(A-B)
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        15.15
+      </td>
+      <td>
+        Weit VE
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+        800 m ZEL(A+B)
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        15.30
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+        1500m A-Lauf
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        15.45
+      </td>
+      <td>
+        1500m A+B
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        16.00
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+        4x100 m ZEL
+      </td>
+      <td>
+      </td>
+      <td>
+        Weit wU16 VE (NP)
+      </td>
+    </tr>
+    <tr>
+      <td>
+        16.15
+      </td>
+      <td>
+        4x100 m ZEL
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        16.30
+      </td>
+      <td>
+        400 m Hü EL (A+B)
+      </td>
+      <td>
+        400 m Hü DLV
+      </td>
+      <td>
+      </td>
+      <td>
+        Weit VE
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        16.45
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+        400 m Hü DLV
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        17.00
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+        200 m ZEL(A-C)
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        17.15
+      </td>
+      <td>
+        200 m ZE A-C (2)
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        17.35
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+        4x400m DLV
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        17.45
+      </td>
+      <td>
+        4x400m DLV
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+    </tr>
+  </table>
 
+  <p style="text-align: center;">
+    VL=Vorlauf, ZVL=Zeitvorlauf,  EL=Endlauf, ZEL=Zeitendläufe, E=Endkampf, VE=Vor/Endkampf
+  </p>
+  <p style="text-align: center;">
+    <strong>
+      Die ersten Vier über 800m/1500m  der Laufnacht qualifizieren sich für die A/B-Läufe der Sparkassen Gala am Samstag
+    </strong>
+  </p>
+<!--
+  <p style="background-color: #FF6600;">
+    <strong>
+      Bei Orange markierten Disziplinen gab es Änderungen im Zeitplan
+    </strong>
+  </p>
+  <p>(x) Reihenfolge der Läufe bei mehreren Altersklassen zur selben Startzeit</p>
+-->
     <?php require("_footer.inc.php"); ?>
