dbec3d31e52873b5991e74e9c597ff00bea74c04
Gerit Wagner <gerit.wagner@wiwi.uni-regensburg.de>
2016-05-18 07:18:12 -0400

    update ladv-link

--- 2016_gala/ausschreibung.php
+++ 2016_gala/ausschreibung.php
@@ -42,7 +42,7 @@
           <br /> Tel. +49 941/43828, Fax +49 941/4607743
           <br /> E-Mail <a href="mailto:meldung@lg-regensburg.de">meldung@lg-regensburg.de</a>
           <br />
-          <br /> Die Meldung ist bevorzugt über das Online Meldeportal <a href="http://ladv.de/ausschreibung/detail/8120/Leichtathletik-Gala-mit-Laufnacht-%285.-6.-Juni%29-Regensburg.htm" target="blank">www.ladv.de</a> durchzuführen. (Registrierung erforderlich)
+          <br /> Die Meldung ist bevorzugt über das Online Meldeportal <a href="http://ladv.de/ausschreibung/detail/10693/Laufnacht-und-Sparkassen-Gala-2016-Regensburg.htm" target="blank">www.ladv.de</a> durchzuführen. (Registrierung erforderlich)
           <br>
           <br> Für alle Läufe bitte Bestleistung 2014/15 angeben, um eine bestmögliche Laufbesetzung ermöglichen zu können.
           <br />
