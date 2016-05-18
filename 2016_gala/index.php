28e61dca98f7b157314140d1670040c7362ae613
Gerit Wagner <gerit.wagner@wiwi.uni-regensburg.de>
2016-05-18 11:46:07 -0400

    update format

--- 2016_gala/index.php
+++ 2016_gala/index.php
@@ -3,10 +3,10 @@
   <?php require("_header.inc.php"); ?>
 
 <h2>Hotelvermittlung Sparkassen Gala und Laufnacht der LG Telis Finanz</h2>
+<br />
 <p>
   <strong>Meetingorganisation hilft beim Buchen von Übernachtungen</strong>
 </p>
-<br />
 <p>Regensburg, 18. Mai 2016 (orv) - Die Organisatoren der Sparkassen Gala und Laufnacht der LG Telis Finanz bieten einen besonderen Service an. Um die Suche nach geeigneten Unterkünften zu erleichtern, übernimmt das Management der Athletenhotels auf Wunsch das Buchen von Selbstzahlern in den jeweiligen Hotels. Bitte wenden Sie sich einfach per Mail an a <a href="mailto:walkner@sparkassen-gala.de">Karin Walkner</a></p>
 
     <?php require("_footer.inc.php"); ?>
