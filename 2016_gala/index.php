5553e33de06d688b5a182f85063fcce3858bef77
Gerit Wagner <gerit.wagner@wiwi.uni-regensburg.de>
2016-05-22 17:57:48 +0200

    zeitplanänderung

--- 2016_gala/index.php
+++ 2016_gala/index.php
@@ -2,9 +2,22 @@
  $menu['current'] = "aktuelles"; ?>
   <?php require("_header.inc.php"); ?>
 
-<h2>Seite der Sparkassen Gala 2016 ist online</h2>
-<br />
-<p>Regensburg, 31. März 2016 - Ab sofort finden Sie an dieser Stelle alle Informationen zur Sparkassen Gala 2016 am 05. Juni im Regensburger Uni Stadion. <!-- <a href="ausschreibung.php">Hier</a> geht's zur Ausschreibung. -->
+<h2>
+3000m bei der Laufnacht für weiblich und männlich
+</h2>
+<br>
+<p>
+  <strong>Ausscheidungsrennen des DLV für die JWM U18</strong>
 </p>
+<p>
+  Regensburg, 22. Mai 2016 (orv) - Wir haben dem Wunsch der Bundestrainer Nachwuchs Langstrecke entsprochen und nun sowohl für den männlichen , als auch den weiblichen Nachwuchs über 3000m bei der Laufnacht der LG Telis Finanz am Samstag, den 4. Juni 2016 postiert. Die Rennen finden um 20.30 Uhr für den weiblichen Nachwuchs und um 20.45 Uhr für den männlichen Nachwuchs statt. Für diese Wettbewerbe kann noch bis 31. Mai 2016 gemeldet werden. Die Wettbewerbe selbst sind DLV-Normwettkämpfe für die Junioren-EM U18 und  WM U20.
+</p>
+
+<h2>Hotelvermittlung Sparkassen Gala und Laufnacht der LG Telis Finanz</h2>
+<br />
+<p>
+  <strong>Meetingorganisation hilft beim Buchen von Übernachtungen</strong>
+</p>
+<p>Regensburg, 18. Mai 2016 (orv) - Die Organisatoren der Sparkassen Gala und Laufnacht der LG Telis Finanz bieten einen besonderen Service an. Um die Suche nach geeigneten Unterkünften zu erleichtern, übernimmt das Management der Athletenhotels auf Wunsch das Buchen von Selbstzahlern in den jeweiligen Hotels. Bitte wenden Sie sich einfach per Mail an a <a href="mailto:walkner@sparkassen-gala.de">Karin Walkner</a></p>
 
     <?php require("_footer.inc.php"); ?>
