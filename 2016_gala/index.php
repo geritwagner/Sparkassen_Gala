0de3dbbbeb7f21a6125d366cd5bef087f85bf6b5
Gerit Wagner <gerit.wagner@wiwi.uni-regensburg.de>
2016-06-01 08:30:14 +0200

    1.6.2016

--- 2016_gala/index.php
+++ 2016_gala/index.php
@@ -2,6 +2,17 @@
  $menu['current'] = "aktuelles"; ?>
   <?php require("_header.inc.php"); ?>
 
+
+<h2>Franzis Gala-Geflüster #2 – Ein Comeback in Regensburg</h2>
+
+<p><strong>Tina Donder – 3000m Hindernis</strong></p>
+
+<p><img src="http://www.lg-telis-finanz.de/images/stories/2014/Donder_Tina_rostock13_foto_gantenberg_8f7469c3a9.jpg" alt="Donder Tina rostock13 foto gantenberg 8f7469c3a9" class="float_left" title="Tina Donder (Foto: Gantenberg)" height="120">Regensburg, 1. Juni 2016 (Reng) - Die erst 20-jährige Tina Donder gehört zu den größten Hindernis-Talenten Deutschlands. Aufgrund von Verletzungspech konnte sie allerdings innerhalb der letzten eineinhalb Jahre kaum Bahnwettkämpfe absolvieren. In Regensburg möchte sie nun endlich wieder ins Geschehen eingreifen: „Das fühlt sich an wie mein erstes Rennen über 3000m Hindernis“, meint sie, „aber jetzt geht es endlich wieder los! Ich denke, dass ich vor allem mental wieder Kraft gesammelt habe und freue mich riesig wieder einzusteigen!“ Für ihr Comeback hat sie sich zunächst einmal die Norm für die Deutschen Meisterschaften als Ziel gesetzt, aber mithilfe der Unterstützung der Zuschauer geht da bestimmt noch ein bisschen mehr!</p>
+
+<h2>Franzis Gala-Geflüster #1</h2>
+
+<p><img src="http://lg-telis-finanz.de/images/stories/2015/Reng-Ring1_Laufnacht15_Bruesselfoto.jpg" alt="Reng Ring1 Laufnacht15 Bruesselfoto" class="float_left" title="Franzi Reng und Kurt Ring (Foto: Brüssel)" height="190">Regensburg, 31. Mai 2016 (Reng) - Hallo liebe Fans, Freunde und Unterstützer der Leichtathletik! Ich bin Franzi Reng und für mich geht gerade ein großer Traum in Erfüllung: Ich darf als Nachwuchs-Journalistin für „die Gala“ schreiben! Damit meine ich allerdings gar nicht die kunterbunte Klatsch-Zeitschrift, sondern die „Sparkassen Gala“: Denn am kommenden Wochenende wird das Regensburger Unistadion wieder zum „Place to be“ für internationale Sportgrößen! Statt von Hollywood-VIPs oder Royals berichte ich also viel lieber von den Stars der Leichtathletik-Szene: Ich werde euch einige Top-Favoriten vorstellen und ihnen ein wenig auf den Zahn fühlen. Die meisten von ihnen absolvieren derzeit noch die letzten intensiven Trainingseinheiten vor ihrem Start. Doch nicht nur bei den Athleten laufen die Wettkampf-Vorbereitungen auf Hochtouren, auch Meeting-Direktor Kurt Ring ist mittlerweile im alljährlichen Gala-Stress. Trotz aller Organisationsarbeit freut er dennoch schon jetzt auf das Event: „Unsere Gala wird immer beliebter. Wir haben hier mit 1100 Teilnehmern alles dabei von den jüngsten Nachwuchs-Talenten bis zu Elite-Sportlern. Die Kleinen können von den Großen lernen, die Großen profitieren von der grandiosen Zuschauerkulisse – das ist einmalig in Deutschland!“</p>
+
 <h2>Bereinigte Teilnehmerliste nun veröffentlicht</h2>
 <p>
   <strong>Daraus sind auch die Einteilungen Vor- und Hauptprogramm ersichtlich</strong>
