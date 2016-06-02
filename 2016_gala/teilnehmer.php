c4cc0b03afb314b59a2454f1813a7d01ae447450
Gerit Wagner <gerit.wagner@wiwi.uni-regensburg.de>
2016-06-02 09:39:16 +0200

    update

--- 2016_gala/teilnehmer.php
+++ 2016_gala/teilnehmer.php
@@ -8,7 +8,7 @@
 
 <!--<p>Die Teilnehmerliste wird im Vorfeld der Sparkassen-Gala auf dieser Seite am 29.05.2016 veröffentlicht.</p>-->
 
-<p>Teilnehmerliste (Stand 1.06.2016) ist <a href="/download/sparkassen_gala_2016_teilnehmer.html" target="_blank">» hier</a> verfügbar.<br />
+<p>Teilnehmerliste (Stand 2.06.2016) ist <a href="/download/sparkassen_gala_2016_teilnehmer.html" target="_blank">» hier</a> verfügbar.<br />
 	Hieraus sind auch die Einteilungen Vor-/ Hauptprogramm ersichtlich. Durch eventuelle Absagen kann es aber noch leichte Verschiebungen geben, wir werden die nächsten Tage noch Updates veröffentlichen.</p>
     <br />
     <!-- <br />Letzter Stand vor der Veranstaltung, kleinere Änderungen sind nur noch am Wettkampftag nach Rücksprache mit der Wettkampfleitung möglich. -->
